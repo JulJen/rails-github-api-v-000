7fbf8e6bd33160ccd471ed1e60b9d6cc6e1705b9
Jul Jen <juliajen.prog@gmail.com>
2018-10-23 00:24:09 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,33 +1,32 @@
 class RepositoriesController < ApplicationController
 
   def index
-  	resp = Faraday.get("https://api.github.com/user/repos") do |req|
-  		req.headers['Authorization'] = "token #{session[:token]}"
-  		req.headers['Accept'] = 'application/json'
-  	end
+    user = Faraday.get 'https://api.github.com/user' do |req|
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
+    @user_data = JSON.parse(user.body)
 
-  	@repos = JSON.parse(resp.body)
+    response = Faraday.get 'https://api.github.com/user/repos' do |req|
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
+    @repos = JSON.parse(response.body)
   end
 
   def create
-  	resp = Faraday.post("https://api.github.com/user/repos") do |req|
-  		req.body = { name: params[:name] }.to_json
-  		req.headers['Authorization'] = "token #{session[:token]}"
-  		req.headers['Accept'] = "application/json"
-  	end
-  	redirect_to root_path
-  	end
-
-  	@repos = JSON.parse(resp.body)
-  end
-
-  def create
-  	resp = Faraday.post("https://api.github.com/user/repos") do |req|
-  		req.body = { name: params[:name] }.to_json
-  		req.headers['Authorization'] = "token #{session[:token]}"
-  		req.headers['Accept'] = "application/json"
-  	end
-  	redirect_to root_path
+    response = Faraday.post 'https://api.github.com/user/repos' do |req|
+      req.body = { 'name': params[:name] }.to_json
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
+    end
+    details = JSON.parse(response.body)
+    if ! response.success?
+      @errors = details["message"] if ! response.success?
+      render :index
+    else
+      redirect_to '/'
+    end
   end
 
 end
